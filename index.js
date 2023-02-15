a34e701c42d02df12869d49da24797263ef465f2
Felix Liawi <felixliawi@gmail.com>
2023-02-15 13:12:06 +0800

    Add console

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
 require('dotenv').config()
 
+console.log("Start")
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
