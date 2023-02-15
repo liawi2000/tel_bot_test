5bf574e462b576f2321e1584f7176f134cff1c31
Felix Liawi <felixliawi@gmail.com>
2023-02-15 13:54:51 +0800

    Use webhook

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
-require('dotenv').config()
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+require("dotenv").config();
 
-console.log("Start")
+console.log("Start");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 bot.launch();
 
+bot.launch({
+  webhook: {
+    domain: "cyclic.sh",
+    port: process.env.PORT || 3000,
+  },
+});
+
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
